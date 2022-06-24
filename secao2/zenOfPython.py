11083a33276624fb1287d49ce3eb0f2019442dd6
Jeferson Luis <jgjefersonluis@gmail.com>
2022-06-24 00:49:10 -0300

    secao2

---
+++ secao2/zenOfPython.py
@@ -0,0 +1,53 @@
+print("O Zen do Python é uma coleção de 19 princípios orientadores.")
+print("Princípios")
+print("Os princípios que constam no Zen of Python são (originalmente em Inglês):")
+
+print("Beautiful is better than ugly.")
+print("Explicit is better than implicit.")
+print("Simple is better than complex.")
+print("Complex is better than complicated.")
+print("Flat is better than nested.")
+print("Sparse is better than dense.")
+print("Readability counts.")
+print("Special cases aren't special enough to break the rules.")
+print("Although practicality beats purity.")
+print("Errors should never pass silently.")
+print("Unless explicitly silenced.")
+print("In the face of ambiguity, refuse the temptation to guess.")
+print("There should be one—and preferably only one—obvious way to do it.")
+print("Although that way may not be obvious at first unless you're Dutch.")
+print("Now is better than never.")
+print("Although never is often better than *right* now.")
+print("If the implementation is hard to explain, it's a bad idea.")
+print("If the implementation is easy to explain, it may be a good idea.")
+print("Namespaces are one honking great idea—let's do more of those!")
+
+print("A versão em Português seria a seguinte:")
+print("Bonito é melhor que feio. ")
+print("Explícito é melhor que implícito.")
+print("Simples é melhor que complexo.  ")
+print("Complexo é melhor que complicado.")
+print("Linear é melhor do que aninhado.")
+print("Esparso é melhor que denso. ")
+print("Legibilidade conta.")
+print("Casos especiais não são especiais o bastante para quebrar as regras.")
+print("Ainda que praticidade vença a pureza.")
+print("Ainda que praticidade vença a pureza.")
+print("Erros nunca devem passar silenciosamente.")
+print("A menos que sejam explicitamente silenciados.")
+print("Diante da ambiguidade, recuse a tentação de adivinhar.")
+print("Dever haver um — e preferencialmente apenas um — modo óbvio para fazer algo.")
+print("Embora esse modo possa não ser óbvio a princípio a menos que você seja holandês.")
+print("Agora é melhor que nunca.")
+print("Apesar de que nunca normalmente é melhor do que *exatamente* agora")
+print("Se a implementação é difícil de explicar, é uma má ideia")
+print("Se a implementação é fácil de explicar, pode ser uma boa ideia")
+print("Namespaces são uma grande ideia — vamos ter mais dessas!")
+
+
+
+
+
+
+
+
