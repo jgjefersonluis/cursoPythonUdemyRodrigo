d495d8512c64ad3891a62839063a878966bb7a9f
Jeferson Luis <jgjefersonluis@gmail.com>
2022-06-26 00:08:06 -0300

    listas

---
+++ secao3/listas/listItems.py
@@ -0,0 +1,11 @@
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1)
+print(list2)
+print(list3)
+
+print(type(list1))
+print(type(list2))
+print(type(list3))
